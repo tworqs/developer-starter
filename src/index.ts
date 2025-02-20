a60f879ba7b5a2bd63a0940c658dfbaef5674dc6
tworqs <tfrullaworks@gmail.com>
2025-02-19 20:31:37 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { greetUser } from '$utils/greet';
 
 window.Webflow ||= [];
 window.Webflow.push(() => {
-  const calendarElement = document.querySelector<HTMLElement>('data-element="calendar"');
+  const calendarElement = document.querySelector<HTMLElement>('[data-element="calendar"]');
   if (!calendarElement) return;
 
   console.log({ calendarElement });
