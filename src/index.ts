0af2f788300e1b03d1a29642dc7e152b10900566
tworqs <tfrullaworks@gmail.com>
2025-04-06 18:22:03 -0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,8 @@ import timeGridPlugin from '@fullcalendar/timegrid';
 
 window.Webflow ||= [];
 window.Webflow.push(() => {
+  console.log('webflow is ready!');
+
   const calendarElement = document.querySelector<HTMLElement>('[data-element="calendar"]');
   if (!calendarElement) return;
 
@@ -12,40 +14,47 @@ window.Webflow.push(() => {
     plugins: [dayGridPlugin, timeGridPlugin, listPlugin],
     initialView: 'dayGridMonth',
     headerToolbar: {
-      left: 'prev,next today',
+      left: 'prev,next today', // add color here
       center: 'title',
-      right: 'dayGridMonth,timeGridWeek,listWeek',
+      right: 'dayGridMonth,timeGridWeek,listWeek', // add color here
     },
     events: [
       {
         title: 'WoM Retreat',
-        start: '2025-02-21',
-        end: '2025-03-03',
-        color: 'orange',
+        start: '2025-04-21',
+        end: '2025-04-30',
+        color: '#FA0985',
+      },
+      {
+        title: 'WoM Site Launch',
+        start: '2025-04-20T13:00',
+        end: '2025-04-20T23:00',
+        color: '#FA0985',
       },
       {
         title: 'Yoga Retreat',
-        start: '2025-02-07',
-        end: '2025-02-10',
-        color: 'pink',
+        start: '2025-04-07',
+        end: '2025-04-10',
+        color: '#DCB684',
       },
       {
         title: 'Health Clinic',
-        start: '2025-02-15T12:00',
-        end: '2025-02-15T23:00',
-        color: 'green',
+        start: '2025-04-15T12:00',
+        end: '2025-04-15T23:00',
+        color: '#2F5C18',
       },
       {
         title: 'Death Cafe',
-        start: '2025-02-15T16:00',
-        end: '2025-02-15T18:00',
-        color: 'purple',
+        start: '2025-04-15T16:00',
+        end: '2025-04-15T18:00',
+        color: '#B7CAE4',
       },
 
       {
         title: 'Tobacco Ceremony',
-        start: '2025-02-18T14:00',
-        end: '2025-02-18T20:00',
+        start: '2025-04-18T14:00',
+        end: '2025-04-18T20:00',
+        color: '#B7CAE4',
       },
     ],
   });
