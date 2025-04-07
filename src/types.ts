1b41d75058ea71364e568613d30aee68f7189a97
tworqs <tfrullaworks@gmail.com>
2025-04-07 14:47:47 -0400

    Update types.ts

--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,6 @@
 export type Event = {
-    title: string;
-    start: string;
-    end: string;
-    location: string;
-
-}
+  title: string;
+  start: string | Date;
+  end: string | Date;
+  location: string;
+};
